1a57ab523754ac9dfe1e4bae1caaaf863403edd2
Axton Smith <axtonsmith@gmail.com>
2022-11-01 22:09:30 -0400

    Add replace and uppercase? methods

--- lib/main.rb
+++ lib/main.rb
@@ -2,12 +2,35 @@
 
 # encodes a given string by a given shift value
 class CaesarCipher
-  attr_reader :alphabet, :string, :integer
+  attr_reader :alphabet, :string, :shift
 
   def initialize(string = 'caesar cipher', integer = 0)
     @alphabet = ('a'..'z').to_a
-    @string = string
-    @integer = integer
+    @string = string.split('')
+    @shift = integer
+  end
+
+  def replace(letter)
+    @alphabet.each_with_index do |value, index|
+      next if letter != value
+
+      new_letter = index + @shift
+      new_letter -= 26 while new_letter > 25
+      new_letter += 26 while new_letter.negative?
+      return alphabet[new_letter]
+    end
+  end
+
+  def uppercase?(letter)
+    return true if ('A'..'Z').to_a.include?(letter)
+
+    return false if ('a'..'z').to_a.include?(letter)
+  end
+
+  def encode
+    new_string = []
+    @string.each do |letter|
+    end
   end
 end
 
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -11,16 +11,45 @@ describe CaesarCipher do
   end
 
   describe '#string' do
-    it 'is a string' do
+    it 'is an array of letters' do
       cipher = CaesarCipher.new
-      expect(cipher.string).to be_a_kind_of String
+      expect(cipher.string).to be_a_kind_of Array
     end
   end
 
-  describe '#integer' do
+  describe '#shift' do
     it 'is an integer' do
       cipher = CaesarCipher.new
-      expect(cipher.integer).to be_a_kind_of Integer
+      expect(cipher.shift).to be_a_kind_of Integer
+    end
+  end
+
+  describe '#replace' do
+    it 'shifts letter by 1' do
+      cipher = CaesarCipher.new('a', 1)
+      expect(cipher.replace('a')).to eql('b')
+    end
+
+    it 'wraps around the alphabet' do
+      cipher = CaesarCipher.new('a', 27)
+      expect(cipher.replace('a')).to eql('b')
+    end
+
+    it 'wraps around the alphabet backwards' do
+      cipher = CaesarCipher.new('a', -25)
+      expect(cipher.replace('a')).to eql('b')
+    end
+  end
+
+  describe '#uppercase?' do
+    it 'returns true when letter is uppercase' do
+      cipher = CaesarCipher.new
+      expect(cipher.uppercase?('A')).to eql(true)
+    end
+
+    it 'returns false when letter is lowercase' do
+      cipher = CaesarCipher.new
+      expect(cipher.uppercase?('a')).to eql(false)
     end
   end
 end

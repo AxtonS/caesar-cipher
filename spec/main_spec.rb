3cf24a277a60347c3d5138327b2f949a649c29aa
Axton Smith <axtonsmith@gmail.com>
2023-10-20 13:05:20 -0400

    Write tests for #shift and #uppercase?

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -2,11 +2,11 @@
 
 require "./lib/main.rb"
 describe CaesarCipher do
-  cipher = CaesarCipher.new
+  subject { CaesarCipher.new  }
   describe "#alphabet" do
     it "should return an array of the alphabet" do
-      expect(cipher.alphabet).to eql(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-    'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
+      expect(subject.alphabet).to eql(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+      'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
     end
   end
 
@@ -17,6 +17,25 @@ describe CaesarCipher do
       it "when 'example'" do
         expect(subject.string).to eql(['e', 'x', 'a', 'm', 'p', 'l', 'e'])
       end
+    end
+  end
+
+  describe "#shift" do
+    context "when 5" do
+      subject { CaesarCipher.new('test', 5)}
+      it "when 5" do
+        expect(subject.shift).to eql(5)
+      end
+    end
+  end
+
+  describe "#uppercase?" do
+    it "should return true if 'A'" do
+      expect(subject.uppercase?('A')).to be true
+    end
+
+    it "should return false if 'z'" do
+      expect(subject.uppercase?('z')).to be false
     end
   end
 end
